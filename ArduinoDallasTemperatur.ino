6b044840a94037dd70b995f9e71257219b3a9715
novprasetio-sys <novprasetio@gmail.com>
2025-12-12 08:01:13 +0700

    Update ArduinoDallasTemperatur.ino

--- ArduinoDallasTemperatur.ino
+++ ArduinoDallasTemperatur.ino
@@ -1,15 +1,11 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 3
-
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
-
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
-
 /*
  * The setup function. We only start the sensors here
  */
@@ -18,11 +14,9 @@ void setup(void)
   // start serial port
   Serial.begin(9600);
   //Serial.println("Dallas Temperature IC Control Library Demo");
-
   // Start up the library
   sensors.begin();
 }
-
 /*
  * Main function, get and show the temperature
  */
@@ -36,7 +30,6 @@ void loop(void)
   // After we got the temperatures, we can print them here.
   // We use the function ByIndex, and as an example get the temperature from the first sensor only.
   float tempC = sensors.getTempCByIndex(0);
-
   // Check if reading was successful
   if(tempC != DEVICE_DISCONNECTED_C) 
   {
